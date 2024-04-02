f9c6bf4b22285eb045d764fcafeea43bf4fc15a9
Ajay <devajay4you@gmail.com>
2024-04-02 08:12:20 +0530

    call911 btn

--- index.js
+++ index.js
@@ -28,4 +28,16 @@ async function orderDrink() {
 
     const drinkOrder = await fetch("https://www.orderadrink.com/userid=8239823")
 
+
+}
+async function call911() {
+
+    try {
+        
+        const call = await fetch("https://www.call911.com/phone=9988392927")
+
+    } catch (error) {
+        
+    }
+
 }
