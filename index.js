51993befcf9a25ab661c34e7b249347e9e683393
Ajay <devajay4you@gmail.com>
2024-04-02 08:10:02 +0530

    orderDrink btn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 console.log("hellow world")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function orderDrink() {
+
+    const drinkOrder = await fetch("https://www.orderadrink.com/userid=8239823")
+
+}
