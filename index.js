b912c3033236f4e78b3237675771f01414d6a701
Ajay <devajay4you@gmail.com>
2024-04-02 08:17:58 +0530

    removed call911 feature

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
 console.log("hellow world")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 async function orderDrink() {
 
     const drinkOrder = await fetch("https://www.orderadrink.com/userid=8239823")
-
-
-}
-async function call911() {
-
-    try {
-        
-        const call = await fetch("https://www.call911.com/phone=9988392927")
-
-    } catch (error) {
-        
-    }
 
 }
