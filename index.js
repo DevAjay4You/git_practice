0f94dd3fe7ea892040e29e809739e18367754919
Ajay <devajay4you@gmail.com>
2024-04-06 11:39:46 +0530

    added callHome logic

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ async function orderDrink() {
     const drinkOrder = await fetch("https://www.orderadrink.com/userid=8239823")
 
 }
+
+async function callHome() {
+    const callHome = await fetch("https://callhome:9928172817281")
+}
